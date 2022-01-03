0e2f59585dbdaee582fc7b200f87a91ebafa2763
AlistairSymonds <alistair.symonds@gmail.com>
2022-01-03 17:57:54 +1100

    codestyle formatting fixes

--- reproject/interpolation/high_level.py
+++ reproject/interpolation/high_level.py
@@ -17,7 +17,8 @@ ORDER['bicubic'] = 3
 @deprecated_renamed_argument('independent_celestial_slices', None, since='0.6')
 def reproject_interp(input_data, output_projection, shape_out=None, hdu_in=0,
                      order='bilinear', independent_celestial_slices=False,
-                     output_array=None, return_footprint=True, output_footprint=None, block_size=None, parallel=False):
+                     output_array=None, return_footprint=True, output_footprint=None,
+                     block_size=None, parallel=False):
     """
     Reproject data to a new projection using interpolation (this is typically
     the fastest way to reproject an image).
@@ -97,16 +98,17 @@ def reproject_interp(input_data, output_projection, shape_out=None, hdu_in=0,
         # block size so each thread gets one block each
         if parallel is not False and block_size is None:
             block_size = [dim // os.cpu_count() for dim in shape_out]
-        
-        #given we have cases where modern system have many cpu cores some sanity clamping is required to avoid 0 
-        #length block sizes when num_cpu_cores is greater than the side of the image
+
+        # given we have cases where modern system have many cpu cores some sanity clamping is
+        # to avoid 0 length block sizes when num_cpu_cores is greater than the side of the image
         for dim_idx in range(len(shape_out)):
             if block_size[dim_idx] == 0:
                 block_size[dim_idx] = shape_out[dim_idx]
 
         return reproject_blocked(_reproject_full, array_in=array_in, wcs_in=wcs_in, wcs_out=wcs_out,
                                  shape_out=shape_out, output_array=output_array, parallel=parallel,
-                                 block_size=block_size, return_footprint=return_footprint, output_footprint=output_footprint)
+                                 block_size=block_size, return_footprint=return_footprint,
+                                 output_footprint=output_footprint)
     else:
         return _reproject_full(array_in, wcs_in, wcs_out, shape_out=shape_out, order=order,
                                array_out=output_array, return_footprint=return_footprint)
